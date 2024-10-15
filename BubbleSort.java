fba2eb2f675d589ba797149d2e1363247bb19b28
Eklas-Mohin <mohin030299@gmail.com>
2024-10-16 01:54:30 +0600

    Basic Sorting Algorithm - Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,12 +2,13 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+        
         System.out.println("Unsorted Array:");
         for (int i = 0; i < arr.length; ++i) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-        
+
         for (int i = 0; i < arr.length - 1; ++i) {
             for (int j = 0; j < arr.length - i - 1; ++j) {
                 if (arr[j] > arr[j + 1]) {
