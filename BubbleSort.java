b9c73ba172207cb643ea895417abee3d89ceb4ff
Eklas-Mohin <mohin030299@gmail.com>
2024-10-16 01:48:26 +0600

    Sorting - Bubble Sort Algorithm

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,8 +2,9 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {17, 23, 7, 59, 73, 13, 37, 53, 47};
-        
-        // Unsorted
+
+        // Print the unsorted array
+        System.out.println("Unsorted Array:");
         for (int i = 0; i < arr.length; ++i) {
             System.out.print(arr[i] + " ");
         }
@@ -12,6 +13,7 @@ public class BubbleSort {
         // Bubble Sort Algorithm
         for (int i = 0; i < arr.length - 1; ++i) {
             for (int j = 0; j < arr.length - i - 1; ++j) {
+                // Swap if the current element is greater than the next element
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -20,7 +22,8 @@ public class BubbleSort {
             }
         }
 
-        // Sorted
+        // Print the sorted array
+        System.out.println("\nSorted Array:");
         for (int i = 0; i < arr.length; ++i) {
             System.out.print(arr[i] + " ");
         }
