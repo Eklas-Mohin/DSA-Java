460cd28548fe63d7dcde355f43aaa2c56222423b
Eklas-Mohin <mohin030299@gmail.com>
2024-10-16 15:34:20 +0600

    Basic Sorting Algorithm - Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,13 +10,16 @@ public class BubbleSort {
         System.out.println();
 
         for (int i = 0; i < arr.length - 1; ++i) {
+            boolean isSwapped = false;
             for (int j = 0; j < arr.length - i - 1; ++j) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    isSwapped = true;
                 }
             }
+            if (!isSwapped) break;
         }
 
         System.out.println("\nSorted Array:");
