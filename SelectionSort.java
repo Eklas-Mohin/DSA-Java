4f33cb382bbcb7064acd2b58eaf46ba60a9e9b40
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 17:00:55 +0600

    Basic Sorting Algorithm - Selection Sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,30 +1,35 @@
 public class SelectionSort {
 
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; ++i) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; ++j) {
+                if (arr[minIdx] > arr[j]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int[] arr = {17, 23, 7, 59, 73, 13, 37, 53, 47};
 
         System.out.println("Unsorted Array:");
-        for (int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        printArray(arr);
 
-        for (int i = 0; i < arr.length - 1; ++i) {
-            int minPos = i;
-            for (int j = i + 1; j < arr.length; ++j) {
-                if (arr[minPos] > arr[j]) {
-                    minPos = j;
-                }
-            }
-            int temp = arr[minPos];
-            arr[minPos] = arr[i];
-            arr[i] = temp;
-        }
+        selectionSort(arr);
 
         System.out.println("\nSorted Array:");
-        for (int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        printArray(arr);
     }
 }
