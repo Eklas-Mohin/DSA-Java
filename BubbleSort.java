09ecba68beba1e80fbafd540b82050d1dfac4921
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 16:56:57 +0600

    Basic Sorting Algorithm - Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,16 +1,9 @@
 public class BubbleSort {
 
-    public static void main(String[] args) {
-        int[] arr = {17, 23, 7, 59, 73, 13, 37, 53, 47};
-        
-        System.out.println("Unsorted Array:");
-        for (int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
+    public static void bubbleSort(int arr[]) {
         for (int i = 0; i < arr.length - 1; ++i) {
             boolean isSwapped = false;
+
             for (int j = 0; j < arr.length - i - 1; ++j) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
@@ -19,13 +12,27 @@ public class BubbleSort {
                     isSwapped = true;
                 }
             }
+
             if (!isSwapped) break;
         }
+    }
 
-        System.out.println("\nSorted Array:");
-        for (int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+
+        System.out.println("Unsorted Array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr);
     }
 }
