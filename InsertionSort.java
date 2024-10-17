6c205d32ba0c0f58fa158b08933390a64cf820d9
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 16:49:09 +0600

    Basic Sorting Algorithm - Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,34 @@
+public class InsertionSort {
+
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; ++i) {
+            int temp = arr[i];
+            int idx = i - 1;
+
+            while (idx >= 0 && arr[idx] > temp) {
+                arr[idx + 1] = arr[idx];
+                idx--;
+            }
+            arr[idx + 1] = temp;
+        }
+    }
+
+    private static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {17, 23, 7, 59, 73, 13, 37, 53, 47};
+
+        System.out.println("Unsorted Array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+}
